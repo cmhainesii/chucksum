3e49c3de4e38714abeb99e8fa005193a655a472d
Charles Haines <charlesmhainesii@gmail.com>
2026-01-12 18:51:29 -0500

    Move checksum functions into savefile

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod savefile;
-mod checksum;
+
 
 
 use savefile::SaveFile;
 
-use crate::checksum::validate_checksum;
+
 
 fn main() -> std::io::Result<()> {
     
@@ -12,31 +12,19 @@ fn main() -> std::io::Result<()> {
 
     println!("Read {} bytes", save_file.len());
 
-    let offset = 0x50;
+    let offset = 0x3000;
     let value = save_file.read_byte(offset);
     println!("0x{:04X}: 0x{:02X}", offset, value);
 
     save_file.write_byte(offset, 66);
+    
 
     let value = save_file.read_byte(offset);
     println!("Afte write byte:");
-    println!("0x{:04X}: {:02X}", offset, value);
+    println!("0x{:04X}: 0x{:02X}", offset, value);
     println!();
 
-
-    let checksum_start = 0x2598;
-    let checksum_end = 0x3522;
-    let checksum_offset = 0x3523;
-
-    let stored_checksum = save_file.read_byte(checksum_offset);
-    let calculated_checksum = checksum::calculate_checksum(save_file.as_slice(), checksum_start, checksum_end);
-    let checksum_valid = validate_checksum(&save_file.as_slice(), calculated_checksum, checksum_offset);
-    if checksum_valid {
-        println!("Checksum ok: 0x{:04X}: {:02X}", checksum_offset, calculated_checksum);
-    }
-    else {
-        println!("Checksum invalid. Stored: {stored_checksum} Calculated: {calculated_checksum}");
-    }
+    
 
     save_file.save("altered.srm")?;
     // let checksum_value = calculate_checksum(&data, checksum_start, checksum_end);
--- src/savefile.rs
+++ src/savefile.rs
@@ -5,6 +5,11 @@ pub struct SaveFile {
 }
 
 impl SaveFile {
+
+    const GEN1_CHECKSUM_START: usize = 0x2598;
+    const GEN1_CHECKSUM_END: usize  = 0x3522;
+    const GEN1_CHECKSUM_OFFSET: usize = 0x3523;
+
     pub fn new(filename: &str) -> std::io::Result<Self> {
 
         let data = fs::read(filename)?;
@@ -29,7 +34,23 @@ impl SaveFile {
         &self.data
     }
 
-    pub fn save(&self, filename: &str) -> std::io::Result<()> {
+    pub fn save(&mut self, filename: &str) -> std::io::Result<()> {
+
+        // Calculate and update checksum. Important, do not skip or file will not be recognized as corrupted by the game
+        self.write_byte(Self::GEN1_CHECKSUM_OFFSET, self.calculate_checksum(
+            Self::GEN1_CHECKSUM_START, Self::GEN1_CHECKSUM_END));
+
+        // Write save data to file
         fs::write(filename, &self.data)
+    }
+
+    fn calculate_checksum(&self, start: usize, end: usize) -> u8 {
+        let mut checksum: u8 = 0;
+
+        for byte in &self.data[start..=end] {
+            checksum = checksum.wrapping_add(*byte);
+        }
+        println!("checksum: 0x{:04X}: 0x{:02X}", Self::GEN1_CHECKSUM_OFFSET, !checksum);
+        !checksum
     }
 }
